d348a01f81d9a52c465a8b901e764f297b95876b
Dane Mackier <dane.mackier@outlook.com>
2023-10-13 12:35:42 +0200

    Updates hash

--- sessionmate.rb
+++ sessionmate.rb
@@ -2,7 +2,7 @@ class Sessionmate < Formula
   desc "Session replay for Flutter to help find and fix bugs in production faster"
   homepage "https://sessionmate.dev/"
   url "https://github.com/FilledStacks/session_mate/releases/download/v.0.2.0/sessionmate.tar.gz"
-  sha256 "4ca5f3e2926ae85eeff61936380449124b2020435d7c8a3066530324711d9409"
+  sha256 "eaa610cbd040bd3f18b6a0540d0c4827acefd7d23c2d3f2a4a037dc01ab0b43b"
   license "MIT"
 
   def install
