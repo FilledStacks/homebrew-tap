a43833959b0841101cecdf49620c8c0787b80d0a
Dane Mackier <dane.mackier@outlook.com>
2023-10-17 07:51:36 +0200

    Adds download strategy

--- sessionmate.rb
+++ sessionmate.rb
@@ -1,9 +1,21 @@
+require_relative "custom_download_strategy.rb"
+
 class Sessionmate < Formula
   desc "Session replay for Flutter to help find and fix bugs in production faster"
   homepage "https://sessionmate.dev/"
-  url "https://github.com/FilledStacks/session_mate/releases/download/v0.3.0/sessionmate.tar.gz"
-  sha256 "49017134f4136be7dd9e36044d31aec0f335b8c7945cdea1b726a5a4c702f607"
   license "MIT"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/FilledStacks/session_mate_cli/releases/download/untagged-53237f7b0858952baddb/sessionmate_macos_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "d03b14e0fb780221cc24702e8832d9535ddd52da3c06bf971193ca4b74e51048"
+    end
+
+    if Hardware::CPU.arm?
+      url "https://github.com/FilledStacks/session_mate_cli/releases/download/untagged-53237f7b0858952baddb/sessionmate_macos_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "fcab8361451502679615af9d76b269ddad186b0c7b6a198eea038a4865a35df7"
+    end
+  end
 
   def install
     bin.install "sessionmate"
