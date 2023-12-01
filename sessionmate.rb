c9144f404186e296c530767c125df9d07c1a7759
Fernando Ferrara <ferrarafer@gmail.com>
2023-12-01 11:54:56 -0300

    chore: update formula to version 0.6.11

--- sessionmate.rb
+++ sessionmate.rb
@@ -1,19 +1,19 @@
 class Sessionmate < Formula
   desc "Session replay for Flutter to help find and fix bugs in production faster"
   homepage "https://sessionmate.dev/"
-  version "0.6.10"
+  version "0.6.11"
   version_scheme 1
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/FilledStacks/session_mate_cli/releases/download/v0.6.10/sessionmate_macos_x86_64.tar.gz"
-      sha256 "7372e8d1c00e79a934c113e8e58f6dc57661b00081e9d1973196f5d202b88ccb"
+      url "https://github.com/FilledStacks/session_mate_cli/releases/download/v0.6.11/sessionmate_macos_x86_64.tar.gz"
+      sha256 "7c549686d36666f64874e8546bbb70aef3925a73b98c3aacbd6de7ced9a8dccb"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/FilledStacks/session_mate_cli/releases/download/v0.6.10/sessionmate_macos_arm64.tar.gz"
-      sha256 "a51b06c1788fc14df06a07939b35c9369199195cb0c46a3c995d9e4360389829"
+      url "https://github.com/FilledStacks/session_mate_cli/releases/download/v0.6.11/sessionmate_macos_arm64.tar.gz"
+      sha256 "f947ebf9f562f71627264030c16ca0f671f100fe5786c09a6d210c38f2c8eca6"
     end
   end
 
