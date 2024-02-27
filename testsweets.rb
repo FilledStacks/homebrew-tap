2dae41919ad6408bd5399599fb3bc0452ab044d8
Dane Mackier <dane.mackier@outlook.com>
2024-02-27 16:53:03 +0200

    Tries to fix path again

--- testsweets.rb
+++ testsweets.rb
@@ -1,19 +1,19 @@
 class Testsweets < Formula
   desc "The easiest way to write automated end-to-end tests for Flutter apps"
   homepage "https://testsweets.com"
-  version "1.0.2"
+  version "1.0.3"
   version_scheme 1
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://firebasestorage.googleapis.com/v0/b/testsweets-38348.appspot.com/o/testsweets-dist.tar.gz?alt=media&token=661aec0b-7505-4bf5-8ad1-09109871c803"
-      sha256 "2350e0b56ee84f6d95acd9e6568bbd1899a129f7429cc468b8ca4d3a299c6ade"
+      url "https://firebasestorage.googleapis.com/v0/b/testsweets-38348.appspot.com/o/testsweets-dist.tar.gz?alt=media&token=9482ee49-ae6b-4578-a817-142f89143253"
+      sha256 "de6881dcee9cb6c8a17af65c7ead4a6896ac82bc22868ea3e588f72f397a0236"
     end
 
     if Hardware::CPU.arm?
-      url "https://firebasestorage.googleapis.com/v0/b/testsweets-38348.appspot.com/o/testsweets-dist.tar.gz?alt=media&token=661aec0b-7505-4bf5-8ad1-09109871c803"
-      sha256 "2350e0b56ee84f6d95acd9e6568bbd1899a129f7429cc468b8ca4d3a299c6ade"
+      url "https://firebasestorage.googleapis.com/v0/b/testsweets-38348.appspot.com/o/testsweets-dist.tar.gz?alt=media&token=9482ee49-ae6b-4578-a817-142f89143253"
+      sha256 "de6881dcee9cb6c8a17af65c7ead4a6896ac82bc22868ea3e588f72f397a0236"
     end
   end
 
