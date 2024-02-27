400ba815cda94a611f716ca585d332dcf6bc2914
Dane Mackier <dane.mackier@outlook.com>
2024-02-27 16:47:40 +0200

    Fixes path to android sdk to use brew --prefix output

--- testsweets.rb
+++ testsweets.rb
@@ -1,19 +1,19 @@
 class Testsweets < Formula
   desc "The easiest way to write automated end-to-end tests for Flutter apps"
   homepage "https://testsweets.com"
-  version "1.0.1"
+  version "1.0.2"
   version_scheme 1
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://firebasestorage.googleapis.com/v0/b/testsweets-38348.appspot.com/o/testsweets-dist.tar.gz?alt=media&token=97ca7490-90ff-44b9-a1ab-c6b553b45ed5"
-      sha256 "39c01adf24d933037a377f3e65762e78d117133840dacf2976a5d88e4069c19b"
+      url "https://firebasestorage.googleapis.com/v0/b/testsweets-38348.appspot.com/o/testsweets-dist.tar.gz?alt=media&token=661aec0b-7505-4bf5-8ad1-09109871c803"
+      sha256 "2350e0b56ee84f6d95acd9e6568bbd1899a129f7429cc468b8ca4d3a299c6ade"
     end
 
     if Hardware::CPU.arm?
-      url "https://firebasestorage.googleapis.com/v0/b/testsweets-38348.appspot.com/o/testsweets-dist.tar.gz?alt=media&token=97ca7490-90ff-44b9-a1ab-c6b553b45ed5"
-      sha256 "39c01adf24d933037a377f3e65762e78d117133840dacf2976a5d88e4069c19b"
+      url "https://firebasestorage.googleapis.com/v0/b/testsweets-38348.appspot.com/o/testsweets-dist.tar.gz?alt=media&token=661aec0b-7505-4bf5-8ad1-09109871c803"
+      sha256 "2350e0b56ee84f6d95acd9e6568bbd1899a129f7429cc468b8ca4d3a299c6ade"
     end
   end
 
