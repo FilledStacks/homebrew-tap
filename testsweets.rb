e9041c8b56bb78f1e3b051f642cff2b1dda7ec84
Dane Mackier <dane.mackier@outlook.com>
2024-02-28 14:45:12 +0200

    ts: Adds build that uses runner sdk path

--- testsweets.rb
+++ testsweets.rb
@@ -7,20 +7,21 @@ class Testsweets < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://firebasestorage.googleapis.com/v0/b/testsweets-38348.appspot.com/o/testsweets-dist.tar.gz?alt=media&token=a5b51656-51c6-41a0-94b9-b7c5ecd7a13f"
-      sha256 "17fde419f25d7178e0e6efb7a51bc5658c7daeff5c1cbbccd4fc250bdaf06c81"
+      url "https://firebasestorage.googleapis.com/v0/b/testsweets-38348.appspot.com/o/testsweets-dist.tar.gz?alt=media&token=c44d53b1-6cdc-4c6f-8175-c61769b0a39a"
+      sha256 "4b84235fcbd08d90c729562650cefa0df7a19dccbc6d0181366cd077e41b4899"
     end
 
     if Hardware::CPU.arm?
-      url "https://firebasestorage.googleapis.com/v0/b/testsweets-38348.appspot.com/o/testsweets-dist.tar.gz?alt=media&token=a5b51656-51c6-41a0-94b9-b7c5ecd7a13f"
-      sha256 "17fde419f25d7178e0e6efb7a51bc5658c7daeff5c1cbbccd4fc250bdaf06c81"
+      url "https://firebasestorage.googleapis.com/v0/b/testsweets-38348.appspot.com/o/testsweets-dist.tar.gz?alt=media&token=c44d53b1-6cdc-4c6f-8175-c61769b0a39a"
+      sha256 "4b84235fcbd08d90c729562650cefa0df7a19dccbc6d0181366cd077e41b4899"
     end
   end
 
   def install
+    bin.install "testsweets"
     # Installs everyting under libexec
-    libexec.install Dir["*"]
-    bin.write_exec_script libexec/"testsweets"
+    # libexec.install Dir["*"]
+    # bin.write_exec_script libexec/"testsweets"
   end
 
   test do
